19a42b0d4236500d98f4523dc651f336edb528f6
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-14 09:10:00 +0530

    Updated Home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
+
+import Shinchan from "../assets/Shinchan.jpg";
 
 export const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <>
-            <div>
-                Home is Working... 
+            <div className="home">
+                <div className="name">
+                    <h1>
+                        Hii, There <br />
+                        I'm Raj Patel.
+                    </h1>
+                    <h2>I'm a Competitive Programmer..</h2>
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => navigate("/about")}
+                    >
+                        About Me
+                    </button>
+                </div>
+                <div className="photo">
+                    <img src={Shinchan} alt="Raj Patel" />
+                </div>
             </div>
         </>
-    )
+    );
 };
