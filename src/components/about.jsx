8dde41bd473622d309cd32bd8f47edb38e85c2d0
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-14 10:15:00 +0530

    Updated About

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
+
+import Raj from "../assets/Raj.jpg";
 
 export const About = () => {
     return (
         <>
-            <div>
-                About is Working... 
+            <div className='about'>
+                <div className="myphoto">
+                    <p>ABOUT ME  ----------</p>
+                    <img src={Raj} alt='Raj Patel'></img>
+                </div>
+                <div className="aboutme">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus totam quasi suscipit, sint sed veniam rem? Sit, deleniti facilis magni enim accusamus ipsam blanditiis quis quos amet similique quidem voluptates!</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime molestias dolorum nihil necessitatibus consectetur veritatis blanditiis pariatur accusantium velit sapiente adipisci et ut, consequuntur, exercitationem minima, tenetur ullam totam suscipit!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt tempora alias asperiores minima id dignissimos quia voluptas similique perspiciatis, repudiandae quisquam excepturi veniam ex voluptatem rerum aspernatur saepe adipisci. Tenetur.</p>
+                </div>
             </div>
         </>
     )
