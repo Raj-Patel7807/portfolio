9c86b221d13c94fe77ba425ec8c6bde465b01c86
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-14 10:32:39 +0530

    Updated Contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./contact.css";
 
 export const Contact = () => {
     return (
         <>
-            <div>
-                Contact is Working... 
+            <div className='contact'>
+                <div className="title">Contact -------</div>
+                <div className="socials">
+                    <a href="#"><i class="fab fa-google"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                    <a href="#"><i class="fab fa-github"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+                <div className="mail"></div>
+                <form className="contact-form">
+                    <div className="form-row">
+                        <input type="text" placeholder="Your Name" required />
+                        <input type="email" placeholder="Your Email" required />
+                    </div>
+                    <input type="text" placeholder="Subject" required />
+                    <textarea placeholder="Message" rows="5" required></textarea>
+                    <button type="submit">Send Message</button>
+                </form>
             </div>
         </>
     )
