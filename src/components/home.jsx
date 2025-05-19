d68c7fc1c352f2442c97acaf8ec10ef982b13379
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-19 22:11:18 +0530

    Updated Home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
 
-import Google from "../assets/Google.svg";
 import Shinchan from "../assets/Shinchan.jpg";
+import Google from "../assets/Google.svg";
+import LinkedIn from "../assets/LinkedIn.svg";
+import Telegram from "../assets/Telegram.svg";
+import GitHub from "../assets/GitHub.svg";
+import CodeForces from "../assets/CodeForces.svg";
+import CodeChef from "../assets/CodeChef.svg";
+import LeetCode from "../assets/LeetCode.svg";
 
 export const Home = () => {
     const navigate = useNavigate();
@@ -18,7 +24,13 @@ export const Home = () => {
                     </h1>
                     <h2>I'm a Competitive Programmer..</h2>
                     <div className="home-socials">
-                        <img src={Google} alt='Google'></img>
+                        <img src={Google} alt="Google" />
+                        <img src={LinkedIn} alt="LinkedIn" />
+                        <img src={Telegram} alt="Telegram" />
+                        <img src={GitHub} alt="GitHub" />
+                        <img src={CodeForces} alt="CodeForces" />
+                        <img src={CodeChef} alt="CodeChef" />
+                        <img src={LeetCode} alt="LeetCode" />
                     </div>
                     <button
                         type="button"
