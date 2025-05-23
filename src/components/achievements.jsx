6d5651fd74b5f32939555ae85f63591cebe0ba75
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-23 17:36:32 +0530

    update: achievements

---
+++ src/components/achievements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Skills = () => {
+    return (
+        <>
+            <div>
+                Achievements is Working... 
+            </div>
+        </>
+    )
+};
