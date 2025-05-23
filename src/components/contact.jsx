43ed5de170dfaa4ff45d352d4710b518330a2081
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-23 10:13:54 +0530

    update: Contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,26 +1,82 @@
-import React from 'react';
+import React from "react";
+
 import "./contact.css";
+
+import Google from "../assets/Google.svg";
+import LinkedIn from "../assets/LinkedIn.svg";
+import Telegram from "../assets/Telegram.svg";
+import GitHub from "../assets/GitHub.svg";
+import CodeForces from "../assets/CodeForces.svg";
 
 export const Contact = () => {
     return (
         <>
-            <div className='contact'>
+            <div className="contact">
                 <div className="title">Contact -------</div>
-                <div className="socials">
-                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                    <a href="#"><i class="fab fa-github"></i></a>
+                <div className="contact-socials">
+                    <div className="social-title">Social Profiles</div>
+                    <div className="socials-icons"> {'>'}
+                        <a
+                            href="https://mail.google.com/mail/?view=cm&to=rajpatel7807@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={Google} alt="Google" title="Google" />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/raj-patel7807/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={LinkedIn} alt="LinkedIn" title="LinkedIn" />
+                        </a>
+                        <a
+                            href="https://t.me/Raj_Patel_7807"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={Telegram} alt="Telegram" title="Telegram" />
+                        </a>
+                        <a
+                            href="https://github.com/Raj-Patel7807"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={GitHub} alt="GitHub" title="GitHub" />
+                        </a>
+                        <a
+                            href="https://codeforces.com/profile/Raj_Patel_7807"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={CodeForces}
+                                alt="CodeForces"
+                                title="CodeForces"
+                            />
+                        </a>
+                    </div>
                 </div>
-                <div className="mail"></div>
+                <div className="mail">
+                    <div className="mail-title">E-mail</div>
+                    <div className="gmail">
+                        {'>'}&nbsp;&nbsp;&nbsp;&nbsp;rajpatel7807@gmail.com
+                    </div>
+                </div>
                 <form className="contact-form">
                     <div className="form-row">
                         <input type="text" placeholder="Your Name" required />
                         <input type="email" placeholder="Your Email" required />
                     </div>
                     <input type="text" placeholder="Subject" required />
-                    <textarea placeholder="Message" rows="5" required></textarea>
+                    <textarea
+                        placeholder="Message"
+                        rows="5"
+                        required
+                    ></textarea>
                     <button type="submit">Send Message</button>
                 </form>
             </div>
         </>
-    )
+    );
 };
