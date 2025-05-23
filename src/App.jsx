d4edb9ba2fc53e1c3402f0f21ee6334b60eb9fdc
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-05-23 17:35:29 +0530

    update: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import './App.css';
+import "./App.css";
 import { Navbar } from "./components/navbar.jsx";
 import { Home } from "./components/home.jsx";
 import { About } from "./components/about.jsx";
-import { Skills } from "./components/skills.jsx";
 import { Education } from "./components/education.jsx";
-import { Work } from "./components/work.jsx";
-import { Experience } from "./components/experience.jsx";
+import { Achievements } from "./components/achievements.jsx";
+import { Projects } from "./components/projects.jsx";
+import { Resume } from "./components/resume.jsx";
 import { Contact } from "./components/contact.jsx";
 
 function App() {
-  return (
-    <>
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/skills" element={<Skills />} />
-                    <Route path="/education" element={<Education />} />
-                    <Route path="/work" element={<Work />} />
-                    <Route path="/experience" element={<Experience />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </div>
-        </Router>
-    </>
-  )
-};
+    return (
+        <>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Routes>
+                        <Route 
+                            path="/" 
+                            element={<Home />} />
+                        <Route 
+                            path="/about" 
+                            element={<About />} />
+                        <Route 
+                            path="/education" 
+                            element={<Education />} />
+                        <Route
+                            path="/achievements"
+                            element={<Achievements />}
+                        />
+                        <Route 
+                            path="/projects" 
+                            element={<Projects />} />
+                        <Route 
+                            path="/resume" 
+                            element={<Resume />} />
+                        <Route 
+                            path="/contact" 
+                            element={<Contact />} />
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    );
+}
 
-export default App
+export default App;
