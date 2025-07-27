d2433c81f8913922bf948c51f4595d0fb649e125
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-07-27 12:01:28 +0530

    update: about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import './about.css';
 
 import Raj from "../assets/Raj.jpg";
@@ -19,10 +22,19 @@ export const About = () => {
                     <div className="about-bio">
                         I am a Second Year B.Tech(ICT) student at Dhirubhai Ambani University (Formerly DA-IICT).
                         Data Structures are my strong suit, and I'm passionate about Competitive Programming and Problem Solving.
-                        As a Coder, I've solved 1300+ Problems across several platforms, including Codeforces, CodeChef and LeetCode.
+                        As a Coder, I've solved 1400+ Problems across several platforms, including Codeforces, CodeChef and LeetCode.
                         Furthermore, I am interested in Web development and have made a few web projects using my Development skills.
-                        Currently I am a 3 star Coder at CodeChef Max Rating(1702), CodeForces (Specialist) Max Rating (1517).
+                        Currently I am a 3 star Coder at CodeChef Max Rating(1611), CodeForces (Specialist) Max Rating (1519).
                     </div>
+                </div>
+                <div className='button'>
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={ () => navigate("/education") }
+                    >
+                        My Education
+                    </button>
                 </div>
             </div>
         </>
