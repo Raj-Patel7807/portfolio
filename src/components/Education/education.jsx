57b298c08ee44d5b49417bfc1199250932b5316f
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 20:52:22 +0530

    update: education

--- src/components/Education/education.jsx
+++ src/components/Education/education.jsx
@@ -7,93 +7,95 @@ export const Education = () => {
     return (
         <>
             <div className="education">
-                <div
-                    className="edu-title"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                >
-                    <h1>
-                        <u>Education</u> 🎓
-                    </h1>
-                </div>
-                <div className="edu-schools">
+                <div className="edu-root">
                     <div
-                        className="school dau"
-                        data-aos="fade-left"
-                        data-aos-duration="1000"
-                    >
-                        <div className="school-logo">
-                            <img src={Images.DAU} alt="DAU" />
-                        </div>
-                        <div className="school-bio">
-                            <p className="school-name">
-                                Dhirubhai Ambani University (Formerly DA-IICT),
-                                Gandhinagar{" "}
-                                <p className="school-batch">2024 - 2028</p>
-                            </p>
-                            <p className="school-course">
-                                B.Tech - Information And Communication
-                                Technology{" "}
-                                <p className="school-score">CGPA: 8.2/10</p>
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        className="school pioneer"
+                        className="edu-title"
                         data-aos="fade-right"
                         data-aos-duration="1000"
                     >
-                        <div className="school-logo">
-                            <img src={Images.Pioneer} alt="Pioneer" />
-                        </div>
-                        <div className="school-bio">
-                            <p className="school-name">
-                                Pioneer School of Science, Patan{" "}
-                                <p
-                                    className="school-batch"
-                                    style={{ margin: "0 0 0 445px" }}
-                                >
-                                    2023 - 2024
-                                </p>
-                            </p>
-                            <p className="school-course">
-                                Class: 12<sup>th</sup>{" "}
-                                <p
-                                    className="school-score"
-                                    style={{ margin: "0 0 0 623px" }}
-                                >
-                                    Percentage: 92.92%
-                                </p>
-                            </p>
-                        </div>
+                        <h1>
+                            <u>Education</u> 🎓
+                        </h1>
                     </div>
-                    <div
-                        className="school pioneer"
-                        data-aos="fade-left"
-                        data-aos-duration="1000"
-                    >
-                        <div className="school-logo">
-                            <img src={Images.Pioneer} alt="Pioneer" />
+                    <div className="edu-schools">
+                        <div
+                            className="school dau"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
+                            <div className="school-logo">
+                                <img src={Images.DAU} alt="DAU" />
+                            </div>
+                            <div className="school-bio">
+                                <p className="school-name">
+                                    Dhirubhai Ambani University (Formerly
+                                    DA-IICT), Gandhinagar{" "}
+                                    <p className="school-batch">2024 - 2028</p>
+                                </p>
+                                <p className="school-course">
+                                    B.Tech - Information And Communication
+                                    Technology{" "}
+                                    <p className="school-score">CGPA: 8.2/10</p>
+                                </p>
+                            </div>
                         </div>
-                        <div className="school-bio">
-                            <p className="school-name">
-                                Pioneer Secondary School, Patan
-                                <p
-                                    className="school-batch"
-                                    style={{ margin: "0 0 0 445px" }}
-                                >
-                                    2021 - 2022
+                        <div
+                            className="school pioneer"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
+                            <div className="school-logo">
+                                <img src={Images.Pioneer} alt="Pioneer" />
+                            </div>
+                            <div className="school-bio">
+                                <p className="school-name">
+                                    Pioneer School of Science, Patan{" "}
+                                    <p
+                                        className="school-batch"
+                                        style={{ margin: "0 0 0 445px" }}
+                                    >
+                                        2023 - 2024
+                                    </p>
                                 </p>
-                            </p>
-                            <p className="school-course">
-                                Class: 10<sup>th</sup>{" "}
-                                <p
-                                    className="school-score"
-                                    style={{ margin: "0 0 0 619px" }}
-                                >
-                                    Percentage: 94.33%
+                                <p className="school-course">
+                                    Class: 12<sup>th</sup>{" "}
+                                    <p
+                                        className="school-score"
+                                        style={{ margin: "0 0 0 623px" }}
+                                    >
+                                        Percentage: 92.92%
+                                    </p>
                                 </p>
-                            </p>
+                            </div>
+                        </div>
+                        <div
+                            className="school pioneer"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
+                            <div className="school-logo">
+                                <img src={Images.Pioneer} alt="Pioneer" />
+                            </div>
+                            <div className="school-bio">
+                                <p className="school-name">
+                                    Pioneer Secondary School, Patan
+                                    <p
+                                        className="school-batch"
+                                        style={{ margin: "0 0 0 445px" }}
+                                    >
+                                        2021 - 2022
+                                    </p>
+                                </p>
+                                <p className="school-course">
+                                    Class: 10<sup>th</sup>{" "}
+                                    <p
+                                        className="school-score"
+                                        style={{ margin: "0 0 0 619px" }}
+                                    >
+                                        Percentage: 94.33%
+                                    </p>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
