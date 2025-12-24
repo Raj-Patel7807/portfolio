5315c32f323b70eb4dd2fed56e8a8140d5fbec73
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 20:37:13 +0530

    update: skills & achievements

--- src/components/Skills/achievements.jsx
+++ src/components/Skills/achievements.jsx
@@ -1,47 +1,82 @@
 import React from "react";
 
 import "./achievements.css";
-import { Images } from "../constants"
+import { Images } from "../constants";
 
 export const Achievements = () => {
     return (
         <>
             <div className="achievements">
                 <div className="ach-root">
-                    <div className="achiev-title" data-aos="fade-right" data-aos-duration="1000">
+                    <div
+                        className="achiev-title"
+                        data-aos="fade-right"
+                        data-aos-duration="1000"
+                    >
                         <h1>
                             <u>Skills</u> 💻
                         </h1>
                     </div>
                     <div className="skills">
-                        <div className="skill" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="skill"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="sk-title">Languages : </p>
                             <p>C, C++</p>
                         </div>
-                        <div className="skill" data-aos="fade-right" data-aos-duration="1000">
+                        <div
+                            className="skill"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
                             <p className="sk-title">Database : </p>
                             <p>xyz..</p>
                         </div>
-                        <div className="skill" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="skill"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="sk-title">Dev Tools : </p>
                             <p>Ubuntu, VS Code, Git, GitHub</p>
                         </div>
-                        <div className="skill" data-aos="fade-right" data-aos-duration="1000">
+                        <div
+                            className="skill"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
                             <p className="sk-title">Area of Interest : </p>
                             <p>DSA, CP, Problem Solving - Setting</p>
                         </div>
-                        <div className="skill" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="skill"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="sk-title">Core Subject : </p>
-                            <p>Data Structure, Design and Analysis of Algo, OOPs</p>
+                            <p>
+                                Data Structure, Design and Analysis of Algo,
+                                OOPs
+                            </p>
                         </div>
                     </div>
-                    <div className="achiev-title" data-aos="fade-right" data-aos-duration="1000">
+                    <div
+                        className="achiev-title"
+                        data-aos="fade-right"
+                        data-aos-duration="1000"
+                    >
                         <h1>
                             <u>Achievements</u> 🎯
                         </h1>
                     </div>
                     <div className="achievs">
-                        <div className="ach" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="ach"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="ac-title">Codeforces : </p>
                             <p>Expert (Max Rating: 1621)</p>
                             <a
@@ -52,7 +87,11 @@ export const Achievements = () => {
                                 [Raj_Patel_7807]
                             </a>
                         </div>
-                        <div className="ach" data-aos="fade-right" data-aos-duration="1000">
+                        <div
+                            className="ach"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
                             <p className="ac-title">CodeChef : </p>
                             <p>3-Star (Max Rating: 1721)</p>
                             <a
@@ -63,7 +102,11 @@ export const Achievements = () => {
                                 [raj_patel_7807]
                             </a>
                         </div>
-                        <div className="ach" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="ach"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="ac-title">LeetCode : </p>
                             <p> (Max Rating: 1675)</p>
                             <a
@@ -74,8 +117,15 @@ export const Achievements = () => {
                                 [Raj_Patel_7807]
                             </a>
                         </div>
-                        <div className="ach" data-aos="fade-right" data-aos-duration="1000">
-                            <p>Solved <b>2200+</b> Coding Problems across the platforms</p>
+                        <div
+                            className="ach"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
+                            <p>
+                                Solved <b>2200+</b> Coding Problems across the
+                                platforms
+                            </p>
                             <a
                                 href="https://codolio.com/profile/Raj_Patel_7807"
                                 target="_blank"
@@ -84,12 +134,23 @@ export const Achievements = () => {
                                 [Profile]
                             </a>
                         </div>
-                        <div className="ach" data-aos="fade-left" data-aos-duration="1000">
+                        <div
+                            className="ach"
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        >
                             <p className="ac-title">JEE (Main) 2024 : </p>
                             <p>98.77 Percentile</p>
                         </div>
-                        <div className="ach" data-aos="fade-right" data-aos-duration="1000">
-                            <p>Secured All Gujarat Rank <b>65</b> in 12th Science</p>
+                        <div
+                            className="ach"
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                        >
+                            <p>
+                                Secured All Gujarat Rank <b>65</b> in 12th
+                                Science
+                            </p>
                         </div>
                     </div>
                 </div>
