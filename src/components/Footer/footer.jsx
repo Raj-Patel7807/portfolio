652e513fb0527dad17b6b08c27bea027a4599d2e
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 21:43:45 +0530

    update: footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -11,13 +11,27 @@ export const Footer = () => {
                     <h3>Raj Patel</h3>
                 </div>
                 <div className="foot-links nav-underline">
-                    <a href="#home" className="nav-link">Home</a>
-                    <a href="#about" className="nav-link">About</a>
-                    <a href="#education" className="nav-link">Education</a>
-                    <a href="#achievements" className="nav-link">Skills & Achievements</a>
-                    <a href="#projects" className="nav-link">Projects</a>
-                    <a href="#resume" className="nav-link">Resume</a>
-                    <a href="#contact" className="nav-link">Contact</a>
+                    <a href="#home" className="nav-link">
+                        Home
+                    </a>
+                    <a href="#about" className="nav-link">
+                        About
+                    </a>
+                    <a href="#education" className="nav-link">
+                        Education
+                    </a>
+                    <a href="#achievements" className="nav-link">
+                        Skills & Achievements
+                    </a>
+                    <a href="#projects" className="nav-link">
+                        Projects
+                    </a>
+                    <a href="#resume" className="nav-link">
+                        Resume
+                    </a>
+                    <a href="#contact" className="nav-link">
+                        Contact
+                    </a>
                 </div>
                 <div className="foot-socials">
                     <a
@@ -25,11 +39,7 @@ export const Footer = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <img
-                            src={Images.Google}
-                            alt="Google"
-                            title="Google"
-                        />
+                        <img src={Images.Google} alt="Google" title="Google" />
                     </a>
                     <a
                         href="https://www.linkedin.com/in/raj-patel7807/"
@@ -58,11 +68,7 @@ export const Footer = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <img
-                            src={Images.GitHub} 
-                            alt="GitHub"
-                            title="GitHub"
-                        />
+                        <img src={Images.GitHub} alt="GitHub" title="GitHub" />
                     </a>
                     <a
                         href="https://codeforces.com/profile/Raj_Patel_7807"
@@ -99,7 +105,7 @@ export const Footer = () => {
                     </a>
                 </div>
                 <div className="copyright">
-                    <p>@2026 Raj Patel. All rights reserved.</p>
+                    <p>©2026 Raj Patel. All rights reserved.</p>
                 </div>
             </footer>
         </>
