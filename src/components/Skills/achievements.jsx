68f25c60615d63ffea1f320668a98dbd9c509c0a
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 10:39:29 +0530

    update: skills

--- src/components/Skills/achievements.jsx
+++ src/components/Skills/achievements.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./achievements.css";
 
-import CodeForces from "../../assets/Socials/CodeForces.png";
+import CodeForces from "../../assets/Socials/CodeForces.svg";
 import CodeChef from "../../assets/Socials/CodeChef.png";
 import LeetCode from "../../assets/Socials/LeetCode.png";
 import Codolio from "../../assets/Socials/Codolio.svg";
@@ -13,150 +13,47 @@ export const Achievements = () => {
             <div className="achievements">
                 <div className="achiev-title" data-aos="fade-down" data-aos-duration="1000">
                     <h1>
+                        <u>Skills</u> 💻
+                    </h1>
+                </div>
+                <div className="skills">
+                    <div className="skill-card">
+                        <img src={CodeForces} alt="React" />
+                        <p>ReactJS</p>
+                    </div>
+
+                    <div className="skill-card">
+                        <img src={CodeForces} alt="React" />
+                        <p>ReactJS</p>
+                    </div>
+
+                    <div className="skill-card">
+                        <img src={CodeForces} alt="React" />
+                        <p>ReactJS</p>
+                    </div>
+
+                    <div className="skill-card">
+                        <img src="/icons/node.svg" alt="Node" />
+                        <p>NodeJS</p>
+                    </div>
+
+                    <div className="skill-card">
+                        <img src="/icons/redux.svg" alt="Redux" />
+                        <p>Redux</p>
+                    </div>
+
+                    <div className="skill-card">
+                        <img src="/icons/firebase.svg" alt="Firebase" />
+                        <p>Firebase</p>
+                    </div>
+                </div>
+                <div className="achiev-title" data-aos="fade-down" data-aos-duration="1000">
+                    <h1>
                         <u>Achievements</u> 🎯
                     </h1>
                 </div>
-                <div className="coding-profiles">
-                    <div className="code-profile codeforces" data-aos="fade-right" data-aos-duration="1000">
-                        <div className="code-profile-logo">
-                            <a
-                                href="https://codeforces.com/profile/Raj_Patel_7807"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={CodeForces}
-                                    alt="Codeforces"
-                                    title="Codeforces"
-                                />
-                            </a>
-                        </div>
-                        <div className="code-profile-bio">
-                            <p className="code-profile-name">
-                                <a
-                                    href="https://codeforces.com/profile/Raj_Patel_7807"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Codeforces
-                                </a>
-                            </p>
-                            <div className="code-profile-rating">
-                                <p>
-                                    1600+ Problems, [ Expert ] (Max Rating :
-                                    1621)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="code-profile codechef" data-aos="fade-left" data-aos-duration="1000">
-                        <div className="code-profile-logo">
-                            <a
-                                href="https://www.codechef.com/users/raj_patel_7807"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={CodeChef}
-                                    alt="CodeChef"
-                                    title="CodeChef"
-                                />
-                            </a>
-                        </div>
-                        <div className="code-profile-bio">
-                            <p className="code-profile-name">
-                                <a
-                                    href="https://www.codechef.com/users/raj_patel_7807"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Codechef
-                                </a>
-                            </p>
-                            <div className="code-profile-rating">
-                                <p>
-                                    100+ Problems, [ ⭐⭐⭐ ] (Max Rating : 1721)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="code-profile leetcode" data-aos="fade-right" data-aos-duration="1000">
-                        <div className="code-profile-logo">
-                            <a
-                                href="https://leetcode.com/u/Raj_Patel_7807/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={LeetCode}
-                                    alt="LeetCode"
-                                    title="LeetCode"
-                                />
-                            </a>
-                        </div>
-                        <div className="code-profile-bio">
-                            <p className="code-profile-name">
-                                <a
-                                    href="https://leetcode.com/u/Raj_Patel_7807/"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Leetcode
-                                </a>
-                            </p>
-                            <div className="code-profile-rating">
-                                <p>500+ Problems, [] (Max Rating : 1675)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="code-profile codolio" data-aos="fade-left" data-aos-duration="1000">
-                        <div className="code-profile-logo">
-                            <a
-                                href="https://codolio.com/profile/Raj_Patel_7807"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={Codolio}
-                                    alt="Codolio"
-                                    title="Codolio"
-                                />
-                            </a>
-                        </div>
-                        <div className="code-profile-bio">
-                            <p className="code-profile-name">
-                                <a
-                                    href="https://codolio.com/profile/Raj_Patel_7807"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Codolio
-                                </a>
-                            </p>
-                            <div className="code-profile-rating">
-                                <p>
-                                    2200+ Problems Solved
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr />
-                <div className="coding-certificate">
-                    <p className="coding-certificate-title" data-aos="fade-right" data-aos-duration="1000"> Certificates </p>
-                    <div className="coding-certi" data-aos="fade-left" data-aos-duration="1000">
-                        <p> ICPC </p>
-                    </div>
-                </div>
-                <hr />
-                <div className="coding-achievement">
-                    <p className="coding-achievement-title" data-aos="fade-right" data-aos-duration="1000"> Contest Achievements </p>
-                    <div className="coding-achieve" data-aos="fade-left" data-aos-duration="1000">
-                        <p>Rank 268 in Codechef Starters 186 (Div. 3)</p>
-                    </div>
-                    <div className="coding-achieve" data-aos="fade-left" data-aos-duration="1000">
-                        <p>Rank 685 in Codeforces Round 1021 (Div. 2)</p>
-                    </div>
+                <div className="achievs">
+
                 </div>
             </div>
         </>
