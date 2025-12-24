b61aa2f5e807d7738e9fe5d2c98e52b69727025c
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 19:36:11 +0530

    update: navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -7,18 +7,42 @@ export const Navbar = () => {
     return (
         <>
             <nav data-aos="fade-down" data-aos-duration="1000">
-                <div className="thumb" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="300">
+                <div
+                    className="thumb"
+                    data-aos="fade-right"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
                     <img src={Images.Shinchan} alt="Thumb" />
                     <p>Raj Patel</p>
                 </div>
-                <div className="links nav-underline" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300">
-                    <a href="#home" className="nav-link">Home</a>
-                    <a href="#about" className="nav-link">About</a>
-                    <a href="#education" className="nav-link">Education</a>
-                    <a href="#achievements" className="nav-link">Skills & Achievements</a>
-                    <a href="#projects" className="nav-link">Projects</a>
-                    <a href="#resume" className="nav-link">Resume</a>
-                    <a href="#contact" className="nav-link">Contact</a>
+                <div
+                    className="links nav-underline"
+                    data-aos="fade-left"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    <a href="#home" className="nav-link">
+                        Home
+                    </a>
+                    <a href="#about" className="nav-link">
+                        About
+                    </a>
+                    <a href="#education" className="nav-link">
+                        Education
+                    </a>
+                    <a href="#achievements" className="nav-link">
+                        Skills & Achievements
+                    </a>
+                    <a href="#projects" className="nav-link">
+                        Projects
+                    </a>
+                    <a href="#resume" className="nav-link">
+                        Resume
+                    </a>
+                    <a href="#contact" className="nav-link">
+                        Contact
+                    </a>
                 </div>
             </nav>
         </>
