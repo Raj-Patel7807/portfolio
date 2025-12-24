6057d844ae1a8318c6c9d170fb82e2db58ff98aa
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-24 21:54:59 +0530

    update: assets

--- src/components/constants.jsx
+++ src/components/constants.jsx
@@ -6,13 +6,13 @@ import Raj from "../assets/Raj.png";
 import DAU from "../assets/Schools/DAU.png";
 import Pioneer from "../assets/Schools/Pioneer.jpg";
 
-import Google from "../assets/Socials/Gmail_4.svg";
-import LinkedIn from "../assets/Socials/LinkedIn_3.svg";
-import Telegram from "../assets/Socials/Telegram_9.svg";
-import GitHub from "../assets/Socials/GitHub_1.svg";
-import CodeForces from "../assets/Socials/CodeForces_1.svg";
-import CodeChef from "../assets/Socials/CodeChef_2.svg";
-import LeetCode from "../assets/Socials/LeetCode_1.svg";
+import Google from "../assets/Socials/Gmail.svg";
+import LinkedIn from "../assets/Socials/LinkedIn.svg";
+import Telegram from "../assets/Socials/Telegram.svg";
+import GitHub from "../assets/Socials/GitHub.svg";
+import CodeForces from "../assets/Socials/CodeForces.svg";
+import CodeChef from "../assets/Socials/CodeChef.svg";
+import LeetCode from "../assets/Socials/LeetCode.svg";
 
 import SnackGame from "../assets/Projects/Snack_Game.png";
 import TetrisGame from "../assets/Projects/Tetris_Game.png";
