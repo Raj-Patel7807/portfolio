611c16c825c49f32cde12ca6cd753d42f872afbb
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-12-25 16:42:30 +0530

    update: navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./navbar.css";
 import { Images } from "../constants";
 
 export const Navbar = () => {
+    const [click, setClick] = React.useState(false);
+    const handleClick = () => {
+        console.log("Menu icon clicked. Current state:", !click);
+        setClick(!click);
+    };
+    const closeMobileMenu = () => {
+        console.log("Link clicked. Closing menu.");
+        setClick(false);
+    };
+
     return (
         <>
             <nav data-aos="fade-down" data-aos-duration="1000">
@@ -16,31 +27,33 @@ export const Navbar = () => {
                     <img src={Images.Shinchan} alt="Thumb" />
                     <p>Raj Patel</p>
                 </div>
+                
+                <div className="menu-icon" onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </div>
+
                 <div
-                    className="links nav-underline"
-                    data-aos="fade-left"
-                    data-aos-duration="1000"
-                    data-aos-delay="100"
+                    className={click ? "links active" : "links"}
                 >
-                    <a href="#home" className="nav-link">
+                    <a href="#home" className="nav-link" onClick={closeMobileMenu}>
                         Home
                     </a>
-                    <a href="#about" className="nav-link">
+                    <a href="#about" className="nav-link" onClick={closeMobileMenu}>
                         About
                     </a>
-                    <a href="#education" className="nav-link">
+                    <a href="#education" className="nav-link" onClick={closeMobileMenu}>
                         Education
                     </a>
-                    <a href="#achievements" className="nav-link">
+                    <a href="#achievements" className="nav-link" onClick={closeMobileMenu}>
                         Skills & Achievements
                     </a>
-                    <a href="#projects" className="nav-link">
+                    <a href="#projects" className="nav-link" onClick={closeMobileMenu}>
                         Projects
                     </a>
-                    <a href="#resume" className="nav-link">
+                    <a href="#resume" className="nav-link" onClick={closeMobileMenu}>
                         Resume
                     </a>
-                    <a href="#contact" className="nav-link">
+                    <a href="#contact" className="nav-link" onClick={closeMobileMenu}>
                         Contact
                     </a>
                 </div>
